f1f3ce222b8b8f51905fd54fafd1c9dbb72f4ae7
ErikBoesen <me@erikboesen.com>
2019-04-02 09:50:11 -0400

    Implement binary tree

--- tree.js
+++ tree.js
@@ -3,4 +3,44 @@ canvas.height = window.innerHeight;
 canvas.width  = window.innerWidth;
 var ctx = canvas.getContext('2d');
 
+function Leaf(value) {
+    this.value = value;
+    this.leftChild = null;
+    this.rightChild = null;
+    this.addLeaf = function(leaf) {
+        if (leaf.value < this.value) {
+            if (this.leftChild === null) {
+                this.leftChild = leaf;
+            } else {
+                this.leftChild.addLeaf(leaf);
+            }
+        } else if (leaf.value >= this.value) {
+            if (this.rightChild === null) {
+                this.rightChild = leaf;
+            } else {
+                this.rightChild.addLeaf(leaf);
+            }
+        }
+    };
+}
+function Tree() {
+    this.root = null;
+    this.insert = function(value) {
+        if (this.root === null) {
+            this.root = new Leaf(value);
+        } else {
+            this.root.addLeaf(new Leaf(value));
+        }
+    };
+}
 
+var tree = new Tree();
+tree.insert(3);
+tree.insert(44);
+tree.insert(32);
+
+function draw(tree) {
+    if (tree.root) {
+
+    }
+}
