6eee3aae9f16c96d58636ee30aff257bbcc7fe84
ErikBoesen <me@erikboesen.com>
2019-04-06 16:13:14 -0400

    Spread exponentially

--- tree.js
+++ tree.js
@@ -4,8 +4,8 @@ canvas.width  = window.innerWidth;
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 ctx.strokeStyle = 'white';
-const NODE_SIZE = 30,
-      X_SPREAD = 20;
+const NODE_SIZE = 20,
+      X_SPREAD = 4;
       Y_MARGIN = 40;
 
 function Leaf(value) {
@@ -31,7 +31,7 @@ function Leaf(value) {
         if (this.leftChild) {
             ctx.fillStyle = 'white';
             ctx.moveTo(x, y);
-            endX = x - this.height * X_SPREAD;
+            endX = x - this.height * Math.pow(2, X_SPREAD);
             endY = y + Y_MARGIN;
             ctx.lineTo(endX, endY);
             this.leftChild.draw(endX, endY);
@@ -39,7 +39,7 @@ function Leaf(value) {
         if (this.rightChild) {
             ctx.fillStyle = 'white';
             ctx.moveTo(x, y);
-            endX = x + this.height * X_SPREAD;
+            endX = x + this.height * Math.pow(2, X_SPREAD);
             endY = y + Y_MARGIN;
             ctx.lineTo(endX, endY);
             this.rightChild.draw(endX, endY);
@@ -47,13 +47,14 @@ function Leaf(value) {
         ctx.fillStyle = 'white';
         ctx.fillRect(x - NODE_SIZE / 2, y, NODE_SIZE, NODE_SIZE);
         ctx.fillStyle = '#333';
-        ctx.fillText(this.value, x, y + 10);
+        //ctx.fillText(this.value, x, y + 10);
+        ctx.fillText(this.height, x, y + 10);
     };
     this.height = null;
     this.calculateHeight = function() {
         if (!this.height) {
-            leftHeight = this.leftChild ? this.leftChild.calculateHeight() + 1 : 0;
-            rightHeight = this.rightChild ? this.rightChild.calculateHeight() + 1 : 0;
+            leftHeight = this.leftChild ? (this.leftChild.calculateHeight() + 1) : 0;
+            rightHeight = this.rightChild ? (this.rightChild.calculateHeight() + 1) : 0;
             this.height = Math.max(leftHeight, rightHeight);
         }
         return this.height;
@@ -71,14 +72,14 @@ function Tree() {
 }
 
 var tree = new Tree();
-for (i = 0; i < 100; i++) {
+for (i = 0; i < 50; i++) {
     tree.insert(Math.floor(Math.random() * 100));
 }
 
 function draw(tree) {
     if (!tree.root) {
     }
-    tree.root.calculateHeight();
+    console.log(tree.root.calculateHeight());
     tree.root.draw(parseInt(window.innerWidth / 2), 20);
     ctx.stroke();
 }
